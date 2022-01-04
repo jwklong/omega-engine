db41124536cc763270dda8f49c94acdf113dc10d
jwklong <72522395+jwklong@users.noreply.github.com>
2022-01-04 11:27:45 +0000

    v0.1.0

--- js/components/changelog-tab.js
+++ js/components/changelog-tab.js
@@ -1,6 +1,11 @@
 Vue.component("changelog-tab", {
     template: `<div class="changelog-tab">
     <guide-item>
+    <template v-slot:title>v0.1.0</template>
+    <template v-slot:text>First public build, with layer names, fonts, the changelog and so much more!
+    </template>
+    <guide-item>
+    <guide-item>
     <template v-slot:title>v0.0.3</template>
     <template v-slot:text>Private Build
     </template>
