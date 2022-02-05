172210a901c1d6a14bcf71873fa8c055c4346922
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-02-05 16:08:19 +0000

    meta fix
    dang

--- js/components/meta-layer.js
+++ js/components/meta-layer.js
@@ -23,7 +23,7 @@ Vue.component("meta-layer", {
         }
     },
     template: `<div class="meta-layer">
-<p class="resource">You have {{formatNumber(metaLayer.resource, 2, 0, 1e9)}}</p>
+<p class="resource">You have {{formatNumber(metaLayer.resource, 2, 0, 1e9)}} <resource-name :layerid="metaLayer.layer.floor()"></resource-name></p>
 <p class="resource alpha" v-if="metaLayer.layer.gt(0)">You have approx. {{formatNumber(metaLayer.getApproxAlpha(), 2, 0, 1e9)}} <resource-name :layerid="0"></resource-name></p>
 <p class="layer">You are on Layer {{formatNumber(metaLayer.layer.add(1), 2, 0, 1e12)}}</p>
 <p>Your resources are multiplied by x{{formatNumber(metaLayer.getMultiPS(), 2, 2)}} each second
