40f414c0c01f9a35165d7923d4412bd72eba315e
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-03-15 17:24:17 +0000

    decimal fix 2

--- js/components/layer-locked-button.js
+++ js/components/layer-locked-button.js
@@ -1,10 +1,11 @@
 Vue.component("layer-locked-button", {
     props: ["layerid"],
     methods: {
-        maxLayerUnlocked: () => game.highestLayer
+        maxLayerUnlocked: () => game.highestLayer,
+        layerdecimalid: () => new Decimal(this.layerid)
     },
-    template: `<button @click="$emit('click')" :disabled="maxLayerUnlocked() < new Decimal(layerid)">
-    <span v-if="maxLayerUnlocked().gte(layerid)">Reach <resource-name :layerid="layerid"></resource-name></span>
+    template: `<button @click="$emit('click')" :disabled="maxLayerUnlocked() < layerdecimalid">
+    <span v-if="maxLayerUnlocked().gte(layerdecimalid)">Reach <resource-name :layerid="layerid"></resource-name></span>
     <span v-else><slot></slot></span>
 </button>`
 })
