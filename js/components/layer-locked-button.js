bf05f6dd36fced1dc78fb5aa9c82c0346a04a6bf
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-03-15 18:19:53 +0000

    Update layer-locked-button.js

--- js/components/layer-locked-button.js
+++ js/components/layer-locked-button.js
@@ -1,11 +1,10 @@
 Vue.component("layer-locked-button", {
     props: ["layerid"],
     methods: {
-        maxLayerUnlocked: () => game.highestLayer,
-        layerdecimalid: () => new Decimal(this.layerid)
+        maxLayerUnlocked: () => game.highestUpdatedLayer,
     },
-    template: `<button @click="$emit('click')" :disabled="maxLayerUnlocked() < layerdecimalid">
-    <span v-if="maxLayerUnlocked().gte(layerdecimalid)">Reach <resource-name :layerid="layerid"></resource-name></span>
+    template: `<button @click="$emit('click')" :disabled="maxLayerUnlocked().gte(layerid)">
+    <span v-if="maxLayerUnlocked().gte(layerid)">Reach <resource-name :layerid="layerid"></resource-name></span>
     <span v-else><slot></slot></span>
 </button>`
 })
