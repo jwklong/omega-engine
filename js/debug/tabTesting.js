cbda5e0c8c2b18f589daad143f5b22637e958996
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-08-29 16:30:45 +0100

    Update and rename TabTesting.js to tabTesting.js

---
+++ js/debug/tabTesting.js
@@ -0,0 +1,10 @@
+mod.debugClasses.push(new DebugClass("Tab Testing", "Test some tabs!", "0.2", [
+    [
+        "Test Endgame Tab",
+        'game.settings.tab = "Endgame"'
+    ],
+    [
+        "Test Secret Achievements Tab",
+        'game.settings.tab = "Secret Achievements"'
+    ]
+]))
