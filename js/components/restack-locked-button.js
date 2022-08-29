33d1aa1e62191629b5451e8db959eecf1224d9f6
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-08-29 12:31:48 +0100

    Update restack-locked-button.js

--- js/components/restack-locked-button.js
+++ js/components/restack-locked-button.js
@@ -4,7 +4,7 @@ Vue.component("restack-locked-button", {
         totalLayerCoins: () => game.restackLayer.layerCoins,
     },
     template: `<button @click="$emit('click')" :disabled="totalLayerCoins().lte(layercoins)">
-    <span v-if="totalLayerCoins().lte(aleph)">Reach {{layercoins}}<img alt="LC" class="inline" src="images/layercoin.png"/></span>
+    <span v-if="totalLayerCoins().lte(aleph)">Reach {{layercoins}}<img alt="LC" class="inline" src="images/layercoin.svg"/></span>
     <span v-else><slot></slot></span>
 </button>`
 })
