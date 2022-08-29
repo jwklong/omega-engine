08f687a942346298252e268d7d93188583be2bf2
sussy layers dev <72522395+jwklong@users.noreply.github.com>
2022-08-29 12:59:45 +0100

    Update achievement.js

--- js/components/achievement.js
+++ js/components/achievement.js
@@ -6,15 +6,11 @@ Vue.component("achievement", {
             showTooltip: false
         }
     },
-    template: `
-    <div v-if="achievement.title != 'linebreak'">
-        <div class="achievement-wrapper">
-            <div class="tooltip" v-if="showTooltip">
-                <h4>{{achievement.title}}</h4>
-                <p v-html="achievement.description"></p>
-            </div>
-            <div @mouseenter="showTooltip = true" @mouseleave="showTooltip = false" :class="{completed: achievement.isCompleted}" class="achievement" v-html="achievement.html"></div>
+    template: `<div class="achievement-wrapper">
+        <div class="tooltip" v-if="showTooltip">
+            <h4>{{achievement.title}}</h4>
+            <p v-html="achievement.description"></p>
         </div>
-     </div>
-     <br v-else>`
+        <div @mouseenter="showTooltip = true" @mouseleave="showTooltip = false" :class="{completed: achievement.isCompleted}" class="achievement" v-html="achievement.html"></div>
+    </div>`
 });
